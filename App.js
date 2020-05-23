eae5e9dfa3c05e3a8e6ea7a5f48f1527eb466867
Franklin Sales <franklin.salesx@gmail.com>
2020-05-23 17:32:18 -0300

    utilize keyExtractor to get the key of item in the list

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
     setCourseGoals(currentGoals => [
       ...currentGoals, 
       { 
-        key: Math.random().toString(), 
+        id: Math.random().toString(), 
         value: enteredGoal
       }
     ]);
@@ -32,7 +32,10 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
 
-      <FlatList data={courseGoals} renderItem={itemData => (
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
         <View style={styles.listItem}> 
           <Text> {itemData.item.value} </Text> 
         </View>
