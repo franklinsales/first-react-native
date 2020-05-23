b08dc753c0c4d3c25e0dc864561b77988ccbf7eb
Franklin Sales <franklin.salesx@gmail.com>
2020-05-23 17:19:12 -0300

    implements scrollview on the list of goals

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -26,12 +26,12 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
 
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => 
           <View style={styles.listItem} key={goal}> 
             <Text> {goal} </Text> 
           </View>)}
-      </View>
+      </ScrollView>
       
     </View>
   );
